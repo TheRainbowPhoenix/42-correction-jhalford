de0ce220e220e39f595d9dc12222e988bc9591d8
Jack Halford <jack@crans.org>
2016-08-03 01:39:34 +0200

    setup usage echo

--- setup.sh
+++ setup.sh
@@ -7,12 +7,22 @@ else
 fi
 
 jhalford-corr () {
-	cd ~/42-correction-jhalford
-	git pull
-	cd -
+	if [ ! -d ~/42-correction-jhalford ]; then
+		git clone https://github.com/jzck/42-correct-jhalford.git ~/42-correction-jhalford
+	else
+		cd ~/42-correction-jhalford
+		git pull
+		cd -
+	fi
 	cp ~/42-correction-jhalford/$1/* .
 	make
 	rm Makefile
 	rm *jhalford.c
 	rm a.out
 }
+
+echo "\e[0;32m"
+echo "        --- Utilisation ---"
+echo "tapez la commande 'jhalford-corr j02'"
+echo "        -------------------"
+echo "\e[0m" 
