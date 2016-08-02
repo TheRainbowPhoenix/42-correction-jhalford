07f6c816a767c7943061a5669562db0d8d2aacab
Jack Halford <jack@crans.org>
2016-08-03 00:37:58 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-git clone https://github.com/jzck/42-correct-jhalford.git ~/jhalford
-jhalford () {
+if [ ! -d ~/jhalford ]
+	git clone https://github.com/jzck/42-correct-jhalford.git ~/jhalford
+fi
+
+jhalford-corr () {
 	cp ~/jhalford/$1/* .
 	make
 }
