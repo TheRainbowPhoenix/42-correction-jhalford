d966b9876b4dd4c5b148bade8915280e9cfd3046
Jack Halford <jack@crans.org>
2016-08-03 11:55:37 +0200

    updt file naming

--- jhalford-corr-install.sh
+++ jhalford-corr-install.sh
@@ -1,6 +1,6 @@
 jhalford-corr () {
 	source ~/42-correction-jhalford/update-git.sh
-	cp ~/42-correction-jhalford/correction_$1\_jhalford.c . 2>/dev/null
+	cp ~/42-correction-jhalford/$1.c . 2>/dev/null
 	cp ~/42-correction-jhalford/Makefile . 2>/dev/null
 	make $1
 	rm Makefile 2>/dev/null
