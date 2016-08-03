21d9ecef50b9c3abe8b5103c831e73b804829bcc
Jack Halford <jack@crans.org>
2016-08-03 15:08:24 +0200

    clone faillllll

--- jhalford-corr-install.sh
+++ jhalford-corr-install.sh
@@ -3,7 +3,7 @@ jhalford-corr () {
 	cp ~/42-correction-jhalford/$1.c .
 	cp ~/42-correction-jhalford/Makefile .
 	make $1
-	# rm Makefile 2>/dev/null
-	# rm j*.c 2>/dev/null
-	# rm a.out 2>/dev/null
+	rm Makefile 2>/dev/null
+	rm j*.c 2>/dev/null
+	rm a.out 2>/dev/null
 }
