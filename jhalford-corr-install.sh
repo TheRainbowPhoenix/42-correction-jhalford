fe1b7a529edde5814cfa180a452d42015a65ac53
Jack Halford <jack@crans.org>
2016-08-03 04:42:34 +0200

    reverting fuckups...

--- jhalford-corr-install.sh
+++ jhalford-corr-install.sh
@@ -1,9 +1,9 @@
 jhalford-corr () {
 	source ~/42-correction-jhalford/update-git.sh
-	cp ~/42-correction-jhalford/correction_$1\_jhalford.c .
-	cp ~/42-correction-jhalford/Makefile .
+	cp ~/42-correction-jhalford/correction_$1\_jhalford.c . 2>/dev/null
+	cp ~/42-correction-jhalford/Makefile . 2>/dev/null
 	make $1
 	rm Makefile 2>/dev/null
-	rm *jhalford.c 2>/dev/null
+	rm j*.c 2>/dev/null
 	rm a.out 2>/dev/null
 }
