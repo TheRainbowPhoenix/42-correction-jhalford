fc8c376474cf08297274010ddcc29a96e97e0c58
Jack Halford <jack@crans.org>
2016-08-03 04:26:01 +0200

    silence rms

--- jhalford-corr-install.sh
+++ jhalford-corr-install.sh
@@ -3,7 +3,7 @@ jhalford-corr () {
 	cp ~/42-correction-jhalford/correction_$1\_jhalford.c .
 	cp ~/42-correction-jhalford/Makefile .
 	make $1
-	rm Makefile
-	rm *jhalford.c
-	rm a.out
+	rm Makefile 2>/dev/null
+	rm *jhalford.c 2>/dev/null
+	rm a.out 2>/dev/null
 }
