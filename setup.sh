5547c88b20d1c2bffae724c23155ecaee4f2a772
Jack Halford <jack@crans.org>
2016-08-03 04:23:32 +0200

    added git clone back to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
+if [ ! -d ~/42-correction-jhalford ]; then
+	git clone https://github.com/jzck/42-correction-jhalford.git ~/42-correction-jhalford
+
 source ~/42-correction-jhalford/update-git.sh
 source ~/42-correction-jhalford/jhalford-corr-install.sh
 
 echo "\e[0;32m"
-echo "             --- Utilisation ---"
-echo "placez vous dans le dosser du jour a corriger"
-echo "    tapez la commande 'jhalford-corr j02'"
-echo "   tapez Entree pour passer a l'exo suivant"
-echo "             -------------------"
-echo "\e[0;34m"
-echo "             ----- Contact -----"
-echo "         slack/intra/ldap : jhalford"
-echo "             -------------------"
-echo "\e[0m"
+echo "        --- Utilisation ---"
+echo "tapez la commande 'jhalford-corr j02'"
+echo "        -------------------"
+echo "\e[0m" 
