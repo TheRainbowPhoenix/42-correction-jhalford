1254d5e33884f680015e8adf33d3f8a9f3fb283f
Jack Halford <jack@crans.org>
2016-08-03 04:31:07 +0200

    vi fucking swp files removed all my good work...

--- setup.sh
+++ setup.sh
@@ -6,7 +6,13 @@ source ~/42-correction-jhalford/update-git.sh
 source ~/42-correction-jhalford/jhalford-corr-install.sh
 
 echo "\e[0;32m"
-echo "        --- Utilisation ---"
-echo "tapez la commande 'jhalford-corr j02'"
-echo "        -------------------"
-echo "\e[0m" 
+echo "             --- Utilisation ---"
+echo "placez vous dans le dosser du jour a corriger"
+echo "    tapez la commande 'jhalford-corr j02'"
+echo "   tapez Entree pour passer a l'exo suivant"
+echo "             -------------------"
+echo "\e[0;34m"
+echo "             ----- Contact -----"
+echo "         slack/intra/ldap : jhalford"
+echo "             -------------------"
+echo "\e[0m"
